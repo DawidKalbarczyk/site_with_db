43ba399373d830e1b09340856e6a295583ff081b
dawid <u56128@student.wat.edu.pl>
2026-04-20 20:52:42 +0200

    fix: fixing stupid errors 4

--- dockerfile/web/web_test.sh
+++ dockerfile/web/web_test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-envsubst '${WEB_TEST}' < /usr/share/nginx/html/index.html.template \
+envsubst '${WEB_TEST} ${ENV_NAME}' < /usr/share/nginx/html/index.html.template \
     > /usr/share/nginx/html/index.html
 
 exec nginx -g 'daemon off;'
