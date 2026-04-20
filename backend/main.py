14d158affa23e010ffb8333598be20c8e95ca90e
dawid <u56128@student.wat.edu.pl>
2026-04-20 21:39:48 +0200

    feat: added fastapi

---
+++ backend/main.py
@@ -0,0 +1,11 @@
+from backend import FastAPI
+
+backend = FastAPI()
+
+@backend.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+@backend.get("/items/{item_id}")
+async def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
