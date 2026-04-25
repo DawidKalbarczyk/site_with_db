34f1e880ec781a862d2c10a37b4d7cd16487f45d
dawid <u56128@student.wat.edu.pl>
2026-04-26 00:50:54 +0200

    test

---
+++ backend/routers/db_insert.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter
+from sqlalchemy import create_engine, text
+router_insert = APIRouter()
+
+from backend.settings import db_name, db_user, db_password
+
+def connect_db(db_name, db_user, db_password):
+    return(create_engine(f"postgresql://{db_user}:{db_password}@db:5432/{db_name}"))
+
+
